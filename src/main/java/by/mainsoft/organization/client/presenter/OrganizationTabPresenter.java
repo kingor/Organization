ed96df8e6325b8a0797af43fe188312a362842c9
kingor <kingor333@mail.ru>
2016-07-22 17:01:58 +0300

    empty EntryPoint class

---
+++ src/main/java/by/mainsoft/organization/client/presenter/OrganizationTabPresenter.java
@@ -0,0 +1,13 @@
+package by.mainsoft.organization.client.presenter;
+
+import com.google.gwt.user.client.ui.HasWidgets;
+
+public class OrganizationTabPresenter implements Presenter {
+
+	@Override
+	public void go(HasWidgets container) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/by/mainsoft/organization/client/presenter/Presenter.java
@@ -0,0 +1,7 @@
+package by.mainsoft.organization.client.presenter;
+
+import com.google.gwt.user.client.ui.HasWidgets;
+
+public interface Presenter {
+	public void go(HasWidgets container);
+}
